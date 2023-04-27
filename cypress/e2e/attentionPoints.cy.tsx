699c632e7f363ed433ae54ea041cb781e8759d59
Igor Gomes <iguinhogms@gmail.com>
2023-04-26 23:26:52 -0300

    cypress v0.6

--- cypress/e2e/attentionPoints.cy.tsx
+++ cypress/e2e/attentionPoints.cy.tsx
@@ -24,12 +24,67 @@ describe('Attention points about Cypress', () => {
   });
 
   it('Confirm', () => {
-    cy.get('#confirm').click();
     cy.on('window:confirm', (msg) => {
       expect(msg).to.be.equal('Confirm Simples');
     });
     cy.on('window:alert', (msg) => {
       expect(msg).to.be.equal('Confirmado');
     });
+    cy.get('#confirm').click();
+  });
+
+  it('Deny', () => {
+    cy.on('window:confirm', (msg) => {
+      expect(msg).to.be.equal('Confirm Simples');
+      return false;
+    });
+    cy.on('window:alert', (msg) => {
+      expect(msg).to.be.equal('Negado');
+    });
+    cy.get('#confirm').click();
+  });
+
+  it('Prompt', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').returns('42');
+    });
+    cy.on('window:confirm', (msg) => {
+      expect(msg).to.be.equal('Era 42?');
+    });
+    cy.on('window:alert', (msg) => {
+      expect(msg).to.be.equal(':D');
+    });
+    cy.get('#prompt').click();
+  });
+});
+
+describe('Desafio', () => {
+  beforeEach('access site wcaquino', () => {
+    cy.visit('https://wcaquino.me/cypress/componentes.html');
+  });
+
+  it.only('Validar formulario', () => {
+    const stub = cy.stub().as('Alert');
+    cy.on('window:alert', stub);
+    cy.get('#formCadastrar')
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio');
+      });
+    cy.get('#formNome').type('Igor');
+    cy.get('#formCadastrar')
+      .click()
+      .then(() => {
+        expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio');
+      });
+    cy.get('[data-cy="dataSobrenome"]').type('Gomes');
+    cy.get('#formCadastrar')
+      .click()
+      .then(() => {
+        expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio');
+      });
+    cy.get('#formSexoMasc').click();
+    cy.get('#formCadastrar').click();
+    cy.get('body').contains('Cadastrado!');
   });
 });
